89a8cd9d551c35409387f5676b871efe6a5a9d7b
William Blacknall <williamblacknall@gmail.com>
2019-10-20 21:41:23 -0400

    Added server generation

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Imports route files
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,10 +18,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//Sets static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Sets path to route folders
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const PORT = process.env.PORT || 5176
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
