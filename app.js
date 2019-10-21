a136fd480a52a4d59680fcb140a554f366e3c6e0
William Blacknall <williamblacknall@gmail.com>
2019-10-21 14:20:31 -0400

    Got handlebars working and learned there are two different handlebars jesus christ

--- app.js
+++ app.js
@@ -1,56 +1,37 @@
 const createError = require('http-errors');
+var exphbs  = require('express-handlebars');
 const express = require('express'),
-  es6Renderer = require('express-es6-template-engine'),
   app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+var hbs = exphbs.create({ /* config */ });
 
 //Imports route files
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', es6Renderer);
-app.set('view engine', 'html');
 
-app.get('/', function(req, res) {
-  res.render('index', {locals: {title: 'Welcome!'}});
+// view engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Renders index page
+app.get('/', function (req, res) {
+  res.render('home');
 });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 //Sets static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Sets path to route folders
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-const PORT = process.env.PORT || 5176
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
 
 
 module.exports = app;
